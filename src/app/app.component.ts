49261c00534a5016e6634a0e4ee632a811d8384b
Diego <d.delajaral@gmail.com>
2021-01-15 12:12:39 -0300

    :sparkles: Add oneSignal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SwPush} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pwa';
+  public title:string = 'angular-pwa'
+  // readonly VAPID_PUBLIC_KEY = "BO2fEF7s0PQqtSUKmaCHS2ECrQl3s8q0NXmD8mxxZU5PV1A6J_SvLgvHf1t3KimaaOU43mWv8E_YPlLjCUtJ3Mc";
+
+  // constructor(
+  //     private swPush: SwPush
+  // ) {}
+
+  // subscribeToNotifications() {
+  //     console.log(1);
+      
+  //     this.swPush.requestSubscription({
+  //         serverPublicKey: this.VAPID_PUBLIC_KEY
+  //     })
+  //     .then(sub => console.log(sub))
+  //     .catch(err => console.error("Could not subscribe to notifications", err));
+  // }
 }
+
